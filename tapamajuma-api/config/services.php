b5822de863683bd6ffc4815647585d20901e505a
radotliveserverdomcloud <radot2510@gmail.com>
2026-02-03 08:13:56 +0000

    rm key

--- tapamajuma-api/config/services.php
+++ tapamajuma-api/config/services.php
@@ -34,8 +34,5 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
-    'gemini' => [
-    'api_key' => env('AIzaSyDLwVjwQ4k73ayUzLBulgKaQDOL6HOc6kk'),
-    ],
 
 ];
