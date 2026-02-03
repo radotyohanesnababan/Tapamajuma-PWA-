3a28a6c55ad0842e724d10e37b601c79f1a5673c
radotliveserverdomcloud <radot2510@gmail.com>
2026-02-03 08:10:08 +0000

    update services

--- tapamajuma-api/config/services.php
+++ tapamajuma-api/config/services.php
@@ -34,5 +34,8 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    'gemini' => [
+    'api_key' => env('AIzaSyDLwVjwQ4k73ayUzLBulgKaQDOL6HOc6kk'),
+    ],
 
 ];
